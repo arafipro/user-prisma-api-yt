fc04a82b030e288e770c8aa99af834d870fbbebb
Hiroki Chiyoda <contact@arafipro.com>
2024-05-14 22:01:09 +0900

    07_create-table

---
+++ prisma/dummy-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO
+	User ("email", "name")
+VALUES
+	('jane@prisma.io', 'Jane Doe'),
+	('john@example.com', 'John Smith'),
+	('alice@company.com', 'Alice Johnson');
---
+++ prisma/migrations/0001_create_user_table.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_name_key" ON "User"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
