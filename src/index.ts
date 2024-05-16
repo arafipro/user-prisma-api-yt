81861f7cd9e0155918d1fec813646d0b7faa8747
Hiroki Chiyoda <contact@arafipro.com>
2024-05-17 08:42:04 +0900

    12_update-data

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { PrismaD1 } from "@prisma/adapter-d1";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, User } from "@prisma/client";
 import { Hono } from "hono";
 
 const app = new Hono<{ Bindings: Bindings }>().basePath("/api");
@@ -26,6 +26,50 @@ app.get("/users/:id", async (c) => {
       where: { id: id },
     });
     return c.json(result);
+  } catch (e) {
+    return c.json({ error: e }, 500);
+  }
+});
+
+app.post("/users", async (c) => {
+  const { name, email } = await c.req.json<User>();
+  const adapter = new PrismaD1(c.env.DB);
+  const prisma = new PrismaClient({ adapter });
+  try {
+    await prisma.user.create({
+      data: { name, email },
+    });
+    return c.json({ message: "Success" }, 201);
+  } catch (e) {
+    return c.json({ error: e }, 500);
+  }
+});
+
+app.put("/users/:id", async (c) => {
+  const id = parseInt(c.req.param("id"));
+  const { name, email } = await c.req.json<User>();
+  const adapter = new PrismaD1(c.env.DB);
+  const prisma = new PrismaClient({ adapter });
+  try {
+    await prisma.user.update({
+      where: { id: id },
+      data: { name, email },
+    });
+    return c.json({ message: "Success" }, 200);
+  } catch (e) {
+    return c.json({ error: e }, 500);
+  }
+});
+
+app.delete("/users/:id", async (c) => {
+  const id = parseInt(c.req.param("id"));
+  const adapter = new PrismaD1(c.env.DB);
+  const prisma = new PrismaClient({ adapter });
+  try {
+    await prisma.user.delete({
+      where: { id: id },
+    });
+    return c.json({ message: "Success" }, 200);
   } catch (e) {
     return c.json({ error: e }, 500);
   }
